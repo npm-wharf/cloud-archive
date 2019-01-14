63d157e917dba4f23d4035a4cabbaaae382051cd
Alexander Early <alex@npmjs.com>
2019-01-14 14:11:26 -0800

    add logging to cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,23 @@
-const { backupFrom, restoreTo } = require('../src/index.js')
-console.log(process.argv, 'ARGUMENTS')
+#!/usr/bin/env node
 
-if (process.argv[2] === 'backup') backupFrom()
-if (process.argv[2] === 'restore') restoreTo()
+const bole = require('bole')
+
+bole.output({
+  level: 'info',
+  stream: process.stdout
+})
+
+const { backupFrom, restoreTo } = require('../src/index.js')
+
+if (process.argv[2] === 'backup') {
+  backupFrom()
+} else if (process.argv[2] === 'restore') {
+  restoreTo()
+} else {
+  console.log(`usage:
+
+  cloud-archive {backup|restore}
+
+(pass config values as environment variables)
+  `)
+}
